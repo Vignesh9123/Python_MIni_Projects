8e9b012a68beb70678dfd50333fcf142c17a60f1
Vignesh9123 <vignesh.d9123@gmail.com>
2022-07-21 14:54:04 +0530

    HC game

--- OTP.py
+++ OTP.py
@@ -5,32 +5,29 @@ print("Hello" , name ,'Please enter the received OTP')
 user_access = False
 while not user_access:
     otp_limit = 0
-    otp = random.randint(100000, 999999)
+    otp = str(random.randint(100000, 999999))
     print('( Your OTP is :' , otp, ')')
-    try :
-       user_entry = int(input('OTP : '))
-    except ValueError:
-       user_entry = int(input("Invalid OTP \nTry Again : "))
-       if user_entry == otp:
-            print("Access granted :)")
-            user_access = True
-       else :
-            user_access = False
-            while not user_access:
-                otp_limit += 1
-                user_entry = int(input("Please enter the correct OTP: "))
-                if user_entry == otp:
-                    print("Access granted :)")
-                    user_access = True
+    user_entry = input('OTP : ')
+    if user_entry == otp:
+        print("Access granted :)")
+        user_access = True
+    else :
+        user_access = False
+        while not user_access:
+            otp_limit += 1
+            user_entry = input("Please enter the correct OTP: ")
+            if user_entry == otp:
+                print("Access granted :)")
+                user_access = True
+                break
+            elif otp_limit >= 2:
+                again = input("Looks like You have got a problem \nWould you like us to send another OTP ?  ")
+                if again == "Yes":
+                    user_access = False
                     break
-                elif otp_limit >= 2:
-                    again = input("Looks like You have got a problem \nWould you like us to send another OTP ?  ")
-                    if again == "Yes":
-                        user_access = False
-                        break
-                    else:
-                        break
-                else :
-                    continue
+                else:
+                    break
+            else :
+                continue
 
 
