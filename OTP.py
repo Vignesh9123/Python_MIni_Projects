83673cd97f9f3fbb0aeb40125a04c3ddd59eb15f
Vignesh9123 <vignesh.d9123@gmail.com>
2022-07-23 22:39:04 +0530

    HC game

--- OTP.py
+++ OTP.py
@@ -6,9 +6,16 @@ user_access = False
 user_limit = 0
 while not user_access and user_limit < 3:
     otp_limit = 0
-    otp = str(random.randint(100000, 999999))
+    otp = random.randint(100000, 999999)
     print('( Your OTP is :' , otp, ')')
-    user_entry = input('OTP : ')
+    while True:
+        try:
+            user_entry = int(input('OTP : '))
+            break
+        except:
+            print("Please enter only numbers")
+            continue
+
     if user_entry == otp:
         print("Access granted :)")
         time.sleep(2)
@@ -17,7 +24,7 @@ while not user_access and user_limit < 3:
         user_access = False
         while not user_access:
             otp_limit += 1
-            user_entry = input("Please enter the correct OTP: ")
+            user_entry = int(input("Please enter the correct OTP: "))
             if user_entry == otp:
                 print("Access granted :)")
                 time.sleep(2)
@@ -26,7 +33,7 @@ while not user_access and user_limit < 3:
             elif otp_limit >= 2:
                 user_limit += 1
                 if user_limit >= 3:
-                    print("Ohh 3 Wrong Attempts!! We would suggest you to try again after some time.\nThank You")
+                    print("Ohh 3 Wrong Attempts!! We would suggest you to try again after some time.\nSorry for the inconvenience.\nThank You")
                     time.sleep(4)
                     sys.exit()
                 again = input("Looks like You have got a problem \nWould you like us to send another OTP ?  ")
